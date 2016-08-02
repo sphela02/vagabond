233cb50bd7eed37c6181ecacbc23699058fbf824
sphela02 <sphelan@Seans-MacBook-Pro.local>
2016-08-02 12:06:33 -0400

    ongoing development of vagrant deployment script

--- vagabond_project_setup.sh
+++ vagabond_project_setup.sh
@@ -1,51 +1,60 @@
-#!/bin/sh
+#!/bin/bash
 
 export whoami=`/usr/bin/whoami`
 
 echo dbg ... whoami = $whoami
 
 # Setup github repos
-if [ ! -d "~/github/hc" ]; then
-    cd ~/github
+if [ ! -d "$HOME/github/hc" ]; then
+    cd $HOME/github
     git clone git@github.com:$whoami/hc.git -b develop
-    cd ~/github/hc
+    cd $HOME/github/hc
     git remote add upstream git@github.com:harris-corp-it/hc.git
 fi
 
 # Setup vagrant to load
-chmod -R 777 ~/github/hc/box
-cd ~/github/hc/box
+chmod -R 777 $HOME/github/hc/box
+cd $HOME/github/hc/box
+#dbg ... Tweak Vagrantfile to make it distinct, or use the local override file to do it
 vagrant up
 
-vagrant ssh <<END_SSH
-cd /var/www/harris
-compser install
-echo COMPOSER INSTALL ... RC = $?
+vagrant ssh -c "cd /var/www/harris/ ; composer install"
+echo $LINENO ... RC = $?
 
-./task.sh setup:git-hooks
-echo SETUP_HOOKS ... RC = $?
+vagrant ssh -c "cd /var/www/harris/ ; ./task.sh setup:git-hooks"
 
-# Install Node
-cd /var/www/harris/sites/all/themes/custom/harris
-./install-node.sh 0.12.9
-source ~/.bashrc
-nvm use --delete-prefix 0.12.9
+vagrant ssh -c "cd /var/www/harris/sites/all/themes/custom/harris ; ./install-node.sh 0.12.9"
 
-#Install gulp and globals
-npm install -g gulp
-npm install -g browser-sync
+vagrant ssh -c 'export NVM_DIR=/home/vagrant/.nvm; . $NVM_DIR/nvm.sh ; cd /var/www/harris/sites/all/themes/custom/harris ; nvm use --delete-prefix 0.12.9 ; npm install -g gulp ; npm install -g browser-sync'
+
+vagrant ssh -c 'export NVM_DIR=/home/vagrant/.nvm; . $NVM_DIR/nvm.sh ; cd /var/www/harris ; ./task.sh frontend:build'
+
+# DBG ... Hack the local.yml here if you're messing the drupal DB settings
+
+vagrant ssh -c 'export NVM_DIR=/home/vagrant/.nvm; . $NVM_DIR/nvm.sh ; cd /var/www/harris ; ./task.sh setup:build:all'
+
+vagrant ssh -c 'export NVM_DIR=/home/vagrant/.nvm; . $NVM_DIR/nvm.sh ; cd /var/www/harris ; ./task.sh setup:drupal:settings'
+
+cp $HOME/.ssh/id_rsa* $HOME/github/hc/box/
+
+vagrant ssh -c 'mv /vagrant/id_rsa* ~/.ssh/'
+
+# dbg - not working?? Maybe the drupaldb hack made trouble
+vagrant ssh -c 'export NVM_DIR=/home/vagrant/.nvm; . $NVM_DIR/nvm.sh ; cd /var/www/harris/docroot ; drush sql-sync --create-db @harris.dev @harris.loc'
+
+
+exit; #dbg
+
 
 # Frontend build
-cd /var/www/harris
-./task.sh frontend:build
+
 echo $LINENO ... RC = $?
 
 # dbg ... change local.yml later to use different db userid and host?
+# dbg perl -pi.orig -e 's/DATE/localtime/e'
 
-./task.sh setup:build:all
 echo $LINENO ... RC = $?
 
-./task.sh setup:drupal:settings
 echo $LINENO ... RC = $?
 
 cd /var/www/harris/docroot
@@ -55,6 +64,4 @@ echo $LINENO ... RC = $?
 drush cc all
 echo $LINENO ... RC = $?
 
-END_SSH
-
-
+ENDSSH
