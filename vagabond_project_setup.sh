802689d6b585c7a1a896ecfa3ecc5844acb0b703
sphela02 <sphelan@Seans-MacBook-Pro.local>
2016-08-12 15:15:41 -0400

    Changed alias sourcing bashrc command to concatenate .bashrc, not overwrite it.

--- vagabond_project_setup.sh
+++ vagabond_project_setup.sh
@@ -46,7 +46,7 @@ echo $LINENO ... RC = $?
 
 # Setup aliases
 grep vagrant ~/.aliases.bash > $HOME/github/hc/box/.aliases
-vagrant ssh -c 'mv /vagrant/.aliases ~/ ; echo "source ~/.aliases" > ~/.bashrc '
+vagrant ssh -c 'mv /vagrant/.aliases ~/ ; echo "source ~/.aliases" >> ~/.bashrc '
 
 vagrant ssh -c 'rsync --archive -e ssh harris.dev@staging-15049.prod.hosting.acquia.com:/mnt/gfs/harris.dev/sites/default/files /var/www/harris/sites/default/' 
 
