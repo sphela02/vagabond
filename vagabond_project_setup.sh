7a194b8c773ab6b1f7350c693db7184f525a1d1f
sphela02 <sphelan@Seans-MacBook-Pro.local>
2016-08-16 13:19:30 -0400

    firmed up git logic to clone and set up repository before setting up the VM.

--- vagabond_project_setup.sh
+++ vagabond_project_setup.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
-export whoami=`/usr/bin/whoami`
+export gitHubUser=sphela02
+export projectInstanceNumber=0
 
-echo dbg ... whoami = $whoami
+########################################
+if [ "$gitHubUser" == "" ]; then
+    export whoami=`/usr/bin/whoami`
+    export gitHubUser=$whoami
+fi
+
+echo dbg ... gitHubUser = $gitHubUser
+
+export gitHubBaseDir=$HOME/github
+export projectDir=$gitHubBaseDir/hc.dev
 
 # Setup github repos
-if [ ! -d "$HOME/github/hc" ]; then
-    cd $HOME/github
-    git clone git@github.com:$whoami/hc.git -b develop
-    cd $HOME/github/hc
+if [ ! -d "$projectDir" ]; then
+    git clone git@github.com:$gitHubUser/hc.git $projectDir -b develop
+
+    cd $projectDir
     git remote add upstream git@github.com:harris-corp-it/hc.git
 fi
 
 # Setup vagrant to load
-chmod -R 777 $HOME/github/hc/box
-cd $HOME/github/hc/box
+chmod -R 777 $projectDir/box
+cd $projectDir/box
 #dbg ... Tweak Vagrantfile to make it distinct, or use the local override file to do it
 vagrant up
 
@@ -35,7 +45,7 @@ vagrant ssh -c 'export NVM_DIR=/home/vagrant/.nvm; . $NVM_DIR/nvm.sh ; cd /var/w
 
 vagrant ssh -c 'export NVM_DIR=/home/vagrant/.nvm; . $NVM_DIR/nvm.sh ; cd /var/www/harris ; ./task.sh setup:drupal:settings'
 
-cp $HOME/.ssh/id_rsa* $HOME/github/hc/box/
+cp $HOME/.ssh/id_rsa* $projectDir/box/
 
 vagrant ssh -c 'mv /vagrant/id_rsa* ~/.ssh/ ; chmod 600 ~/.ssh/id_rsa*'
 
@@ -45,7 +55,7 @@ vagrant ssh -c 'export NVM_DIR=/home/vagrant/.nvm; . $NVM_DIR/nvm.sh ; cd /var/w
 echo $LINENO ... RC = $?
 
 # Setup aliases
-grep vagrant ~/.aliases.bash > $HOME/github/hc/box/.aliases
+grep vagrant ~/.aliases.bash > $projectDir/box/.aliases
 vagrant ssh -c 'mv /vagrant/.aliases ~/ ; echo "source ~/.aliases" >> ~/.bashrc '
 
 vagrant ssh -c 'rsync --archive -e ssh harris.dev@staging-15049.prod.hosting.acquia.com:/mnt/gfs/harris.dev/sites/default/files /var/www/harris/sites/default/' 
